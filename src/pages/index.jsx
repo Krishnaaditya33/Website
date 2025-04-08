7a97ae6e30452368430e1d348b435e921ef4d1e4
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-08 10:07:04 +0530

    Create index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+// pages/index.jsx
+import FAQ from '@/components/Landing/FAQ'
+
+// In your Home component
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+      {/* ... other components ... */}
+      <Pricing />
+      <FAQ />
+      <Footer />
+    </div>
+  )
+}
